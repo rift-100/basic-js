8df5a6492a34fb96637a26eb329ee3da2770a5b6
Jegres Ok <exp33int@gmail.com>
2025-01-05 15:06:10 +0500

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeatStr(str, count, separator) {
+  return new Array(count).fill(str).join(separator);
+}
+
+function repeater(str, options) {
+  let newStr = String(str);
+  let addition = options.hasOwnProperty("addition")
+    ? String(options.addition)
+    : "";
+  let repeatTimes = 1;
+  if (
+    typeof options.repeatTimes === "number" &&
+    options.repeatTimes % 1 === 0
+  ) {
+    repeatTimes = options.repeatTimes;
+  }
+  let additionRepeatTimes = 1;
+  if (
+    typeof additionRepeatTimes === "number" &&
+    options.additionRepeatTimes % 1 === 0
+  ) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  let separator = "+";
+  if (options.hasOwnProperty("separator")) {
+    separator = String(options.separator);
+  }
+  let additionSeparator = "|";
+  if (options.hasOwnProperty("additionSeparator")) {
+    additionSeparator = String(options.additionSeparator);
+  }
+  newStr += repeatStr(addition, additionRepeatTimes, additionSeparator);
+  return repeatStr(newStr, repeatTimes, separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
